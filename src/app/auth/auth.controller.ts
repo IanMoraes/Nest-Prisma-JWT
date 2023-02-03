828e23ee4bce964617b20673a139f1f4fb3e87f1
IanMoraes <ianmoraes1@gmail.com>
2023-02-03 08:30:30 -0300

    init

--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -1,22 +1,22 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { Public } from 'src/decotrators/public.decorator';
+import { Body, Controller, Post } from "@nestjs/common";
+import { Public } from "src/decorators/public.decorator";
 
-import { AuthService } from './auth.service';
-import { AuthLoginDto } from './dto/auth-login.dto';
-import { CreateUserDto } from './dto/create-user.dto';
+import { AuthService } from "./auth.service";
+import { AuthLoginDto } from "./dto/auth-login.dto";
+import { CreateUserDto } from "./dto/create-user.dto";
 
 @Controller()
 export class AuthController {
-  constructor(private readonly authService: AuthService) { }
+  constructor(private readonly authService: AuthService) {}
 
   @Public()
-  @Post('/register')
+  @Post("/register")
   create(@Body() createUserDto: CreateUserDto) {
     return this.authService.create(createUserDto);
   }
 
   @Public()
-  @Post('/login')
+  @Post("/login")
   async login(@Body() authLoginDto: AuthLoginDto) {
     return this.authService.login(authLoginDto);
   }
